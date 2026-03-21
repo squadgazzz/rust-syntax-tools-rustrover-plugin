7e809aeb4e6d9393b42661ea1eb3cd7b82e5e5c9
squadgazzz <squad.gazzz@gmail.com>
2026-03-21 09:34:14 +0000

    feat: add hourglass gutter icon SVGs and icon loader

---
+++ src/main/kotlin/com/github/squadgazzz/rustsyntaxtoolsrustroverplugin/icons/PluginIcons.kt
@@ -0,0 +1,8 @@
+package com.github.squadgazzz.rustsyntaxtoolsrustroverplugin.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object PluginIcons {
+    @JvmField
+    val Hourglass = IconLoader.getIcon("/icons/hourglass.svg", PluginIcons::class.java)
+}
